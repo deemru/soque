b41f805ad78ee283314cd128a3d28d5b015283af
Dmitrii Pichulin <deem@deem.ru>
2016-04-05 12:22:02 +0300

    naming: soque_test

--- build_windows/make.bat
+++ build_windows/make.bat
@@ -9,4 +9,4 @@ cl /c /Ox /Os /GL /GF /GS- /W4 /EHsc /I../src ../examples/soque_test.c
 rc -r soque.rc
 
 link /DLL /LTCG soque.obj soque.res /OUT:%OUT%.dll
-link /LTCG soque_test.obj soque.res %OUT%.lib /subsystem:console /OUT:%OUT%.exe
+link /LTCG soque_test.obj soque.res %OUT%.lib /subsystem:console /OUT:%OUT%_test.exe
